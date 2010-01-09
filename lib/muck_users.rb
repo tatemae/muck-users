fdc8645f3a991ebdb95c23ec737a1e5b73b05100
Justin Ball <justinball@gmail.com>
2010-01-09 16:40:03 -0700

    adding user admin nav

--- lib/muck_users.rb
+++ lib/muck_users.rb
@@ -8,3 +8,6 @@ ActiveRecord::Base.class_eval { include MuckUsers::Exceptions }
 ActionController::Base.send :helper, MuckUsersHelper
 
 I18n.load_path += Dir[ File.join(File.dirname(__FILE__), '..', 'locales', '*.{rb,yml}') ]
+
+# Add a link to admin users page
+MuckEngine.add_muck_admin_nav_item(I18n.translate('muck.users.users_admin'), '/admin/users', '/images/admin/user.gif')
